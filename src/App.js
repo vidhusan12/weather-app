027d3157d0239c4e728cba2cc534148fbd2a464f
vidhusan12 <vidhusan709@gmail.com>
2025-06-14 17:27:10 +1200

    Added fetch to get Weather Data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 
 function App() {
@@ -12,6 +12,19 @@ function App() {
   //hanlde search submit function
   function handleSearchSubmit(e) {
     e.preventDefault();
+
+    const apiKey = "1e7677f38abca773b7f6c9fab0e53dae";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log("Error fetching weather:", error);
+      });
+
     console.log("Searching for weather in:", city);
   }
 
